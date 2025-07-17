c558905f87c34907464d2030f2cf14b817dec171
shivigupta895 <69303438+shivigupta895@users.noreply.github.com>
2025-07-17 17:02:45 +0530

    Add files via upload

--- payment-service/main.go
+++ payment-service/main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"payment-service/config"
 	"payment-service/pubsub"
 )
 
-func main() {
+func startPaymentService(w http.ResponseWriter, r *http.Request) {
 	db := config.InitDB()
 	log.Println("Starting Payment Service...")
 
 	pubsub.SubscribeToOrderEvents(db)
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Payment Service started and subscribed to order events."))
+}
+
+func main() {
+	http.HandleFunc("/start-payment-service", startPaymentService)
+
+	log.Println("Server is running on port 8081...")
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
