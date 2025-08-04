c8fd8c3731acf52cf041617cdb22b061653d15f8
shivigupta895 <69303438+shivigupta895@users.noreply.github.com>
2025-08-04 14:23:26 +0530

    Add files via upload

--- auth-service/main.go
+++ auth-service/main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"auth-service/routes"
-	// "auth-service/utils"
 )
 
 func main() {
-	// utils.LoadEnvVariables()
-
 	r := routes.SetupRouter()
 	r.Run(":8082") // Starts server on localhost:8082
 }
