724b5189db028a0ddc6e77ea379a4c550996ce2d
shivigupta895 <69303438+shivigupta895@users.noreply.github.com>
2025-08-07 19:20:19 +0530

    Add files via upload

--- payment-service/handlers/payment_handler.go
+++ payment-service/handlers/payment_handler.go
@@ -3,16 +3,11 @@ package handlers
 import (
 	"log"
 	"net/http"
-	"payment-service/config"
-	"payment-service/pubsub"
 
 	"github.com/gin-gonic/gin"
 )
 
 func PaymentHandler(c *gin.Context) {
-	db := config.InitDB()
 	log.Println(`{"message":"Inside payment handler", "service":"payment", "severity":"INFO"}`)
-
-	pubsub.SubscribeToOrderEvents(db)
 	c.JSON(http.StatusOK, gin.H{"message": "Payment Service started and subscribed to order events"})
 }
